e7d7c05173a3e034e3b6e93e1bce44d37da2c7d9
zheng808 <lizheng808@gmail.com>
2014-09-27 00:57:17 -0700

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by lizheng on 2014-04-28.
+//  Copyright (c) 2014 lizheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
