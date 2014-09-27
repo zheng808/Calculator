21d0775d603cc7f642a8b216e3f83f2668af2bac
zheng808 <lizheng808@gmail.com>
2014-09-27 00:55:02 -0700

    Create main.m

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by lizheng on 2014-04-28.
+//  Copyright (c) 2014 lizheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
